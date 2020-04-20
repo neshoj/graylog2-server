e9fc65fee10eb1e3cdf00e01bdf634b84b5e9bcf
Konrad Merz <konrad@graylog.com>
2020-04-20 13:51:25 +0200

    Use navigationItems key for PluginStore

--- graylog2-web-interface/src/components/navigation/Navigation.jsx
+++ graylog2-web-interface/src/components/navigation/Navigation.jsx
@@ -67,6 +67,7 @@ const Navigation = ({ permissions, fullName, location, loginName }) => {
   const pluginNavigations = PluginStore.exports('navigation')
     .sort((route1, route2) => naturalSort(route1.description.toLowerCase(), route2.description.toLowerCase()))
     .map((pluginRoute) => formatPluginRoute(pluginRoute, permissions, location));
+  const pluginItems = PluginStore.exports('navigationItems');
 
   return (
     <StyledNavbar inverse fluid fixedTop>
@@ -77,7 +78,8 @@ const Navigation = ({ permissions, fullName, location, loginName }) => {
           </LinkContainer>
         </Navbar.Brand>
         <Navbar.Toggle />
-        <HeaderBadge />
+        <HeaderBadge smallScreen />
+        {pluginItems.map((Item) => <Item smallScreen />)}
       </Navbar.Header>
 
       <Navbar.Collapse>
@@ -110,6 +112,7 @@ const Navigation = ({ permissions, fullName, location, loginName }) => {
         <Nav navbar pullRight className="header-meta-nav">
           <InactiveNavItem className="dev-badge-wrap">
             <HeaderBadge />
+            {pluginItems.map((Item) => <Item />)}
           </InactiveNavItem>
 
           <LinkContainer to={Routes.SYSTEM.NODES.LIST}>
